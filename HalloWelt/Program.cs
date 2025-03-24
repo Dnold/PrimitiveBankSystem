20dafff3840c03b99d1fdfb9257872d4393c8a9f
Daniel Nold <75209804+Dnold@users.noreply.github.com>
2025-03-24 16:04:20 +0100

    Add Dictonary and Navigation

--- HalloWelt/Program.cs
+++ HalloWelt/Program.cs
@@ -6,14 +6,6 @@ public class Program
     public static void Main(string[] args)
     {
         BankInterface.PrintWelcomeScreen();
-
-
-
-
-
-
-
-
     }
 }
 public static class BankInterface
@@ -21,23 +13,24 @@ public static class BankInterface
     public static void PrintUserInterface(Account account)
     {
         Console.Clear();
+        Console.WriteLine("Home >> Login >> My Account");
         Console.WriteLine($"Welcome, {account.ownerName}!");
         Console.WriteLine($"Your current balance: {account.balance} €");
 
         Console.WriteLine("1. Transfer Money");
         Console.WriteLine("2. Logout");
 
-        string option = Console.ReadLine();
+        string? option = Console.ReadLine();
         switch (option)
         {
-          
+
             case "1":
                 PrintTransferMoney(account);
                 break;
             case "2":
                 PrintWelcomeScreen();
                 break;
-            
+
             default:
                 PrintUserInterface(account);
                 break;
@@ -46,6 +39,7 @@ public static class BankInterface
     private static void PrintTransferMoney(Account sender)
     {
         Console.Clear();
+        Console.WriteLine("Home >> Login >> My Account >> Transfer Money");
         Console.Write("Enter recipient's IBAN: ");
         if (!int.TryParse(Console.ReadLine(), out int recipientIBAN))
         {
@@ -64,16 +58,22 @@ public static class BankInterface
             return;
         }
 
-        BankSystem.PerformTransaction(sender.IBAN, recipientIBAN, amount);
-        Console.WriteLine("Transfer successful!");
-        Console.WriteLine("Press any key to continue...");
+        if (BankSystem.PerformTransaction(sender.IBAN, recipientIBAN, amount))
+        {
+            Console.WriteLine("Transfer was successful!");
+        }
+        else
+        {
+            Console.WriteLine("Transfer was not successful! ");
+        }
+            Console.WriteLine("Press any key to continue...");
         Console.ReadKey();
         PrintUserInterface(sender);
     }
     public static void PrintLoginScreen()
     {
         Console.Clear();
-        Console.WriteLine("Login");
+        Console.WriteLine("Home >> Login");
         Console.Write("Enter your IBAN: ");
         int iban;
         if (!int.TryParse(Console.ReadLine(), out iban))
@@ -85,12 +85,13 @@ public static class BankInterface
         }
 
         Console.Write("Enter your password: ");
-        string password = Console.ReadLine();
+        string? password = Console.ReadLine();
 
-        Account account = BankSystem.Authenticate(iban, password);
+        Account? account = BankSystem.Authenticate(iban, password);
+        Console.Clear();
+        Console.WriteLine("Home >> Login");
         if (account != null)
         {
-            Console.Clear();
             Console.WriteLine("Login successful! Press any key to continue...");
             Console.ReadKey();
             PrintUserInterface(account);
@@ -114,7 +115,7 @@ public static class BankInterface
         Console.WriteLine("2. Create New Bank");
         Console.WriteLine("3. Login into Account");
 
-        string option = Console.ReadLine();
+        string? option = Console.ReadLine();
         switch (option)
         {
             case "1":
@@ -137,16 +138,17 @@ public static class BankInterface
     public static void PrintCreateNewAccount()
     {
         Console.Clear();
-        Console.WriteLine("Create new Account");
+        Console.WriteLine("Home >> Create Account");
         Console.WriteLine("Enter your prefered action");
         Console.WriteLine("1. Create Account");
         Console.WriteLine("2. Go Back to Home");
 
-        string option = Console.ReadLine();
+        string? option = Console.ReadLine();
         switch (option)
         {
             case "1":
                 Console.Clear();
+                Console.WriteLine("Home >> Create Account");
                 if (!BankSystem.AreBanksAvailable())
                 {
                     Console.WriteLine("Sorry there arent any Banks available... please create a bank");
@@ -154,21 +156,21 @@ public static class BankInterface
                     Console.ReadKey();
                     PrintWelcomeScreen();
                 }
-                Console.WriteLine("Create new Account");
+                
                 Console.WriteLine("Enter the BIC of the Bank you want a account at:");
                 byte chosenBIC = Convert.ToByte(Console.ReadLine());
                 if (BankSystem.IsBICTaken(chosenBIC))
                 {
                     Console.Clear();
-                    Console.WriteLine("Create new Account");
+                    Console.WriteLine("Home >> Create Account");
                     Console.WriteLine("What is the Name of the Owner of the account");
-                    string chosenName = Console.ReadLine();
+                    string? chosenName = Console.ReadLine();
                     Console.Clear();
-                    Console.WriteLine("Create new Account");
+                    Console.WriteLine("Home >> Create Account");
                     Console.WriteLine("Choose a Password for the account");
-                    string chosenPassword = Console.ReadLine();
+                    string? chosenPassword = Console.ReadLine();
                     Console.Clear();
-                    Console.WriteLine("Create new Account");
+                    Console.WriteLine("Home >> Create Account");
                     Console.WriteLine("Confirm your Details");
                     Console.WriteLine("BIC:" + chosenBIC);
                     Console.WriteLine("Owner Name: " + chosenName);
@@ -177,13 +179,20 @@ public static class BankInterface
                     Console.WriteLine("Enter your prefered action");
                     Console.WriteLine("1. Confirm Account");
                     Console.WriteLine("2. Go Back to Home");
-                    string option2 = Console.ReadLine();
+                    string? option2 = Console.ReadLine();
                     if (option2 == "1")
                     {
-                        Bank choosenBank = BankSystem.GetBankByBIC(chosenBIC);
-                        Account newAcc = choosenBank.CreateAccount(chosenName, chosenPassword);
-                        Console.Clear(); Console.WriteLine("Your Account has been created");
-                        Console.WriteLine("Your IBAN is: " + newAcc.IBAN);
+                        Bank? choosenBank = BankSystem.GetBankByBIC(chosenBIC);
+                        Account? newAcc = null;
+                        if(choosenBank != null)
+                        {
+                            newAcc = choosenBank.CreateAccount(chosenName, chosenPassword);
+                        }
+                        
+                        Console.Clear();
+                        Console.WriteLine("Home >> Create Account");
+                        Console.WriteLine("Your Account has been created");
+                        Console.WriteLine("Your IBAN is: " + newAcc?.IBAN);
                         Console.WriteLine("Press any Key to continue...");
                         Console.ReadKey();
                         PrintWelcomeScreen();
@@ -200,8 +209,8 @@ public static class BankInterface
                     Console.ReadKey();
                     PrintWelcomeScreen();
                 }
-                    break;
-            
+                break;
+
             default:
                 PrintWelcomeScreen(); break;
 
@@ -210,17 +219,17 @@ public static class BankInterface
     public static void PrintCreateNewBank()
     {
         Console.Clear();
-        Console.WriteLine("Create New Bank");
+        Console.WriteLine("Home >> Create New Bank");
         Console.WriteLine("Enter your prefered action");
         Console.WriteLine("1. Create Bank");
         Console.WriteLine("2. Go Back to Home");
 
-        string option = Console.ReadLine();
+        string? option = Console.ReadLine();
         switch (option)
         {
             case "1":
                 Console.Clear();
-                Console.WriteLine("Create New Bank");
+                Console.WriteLine("Home >> Create New Bank");
                 Console.WriteLine("Enter a BIC for the new Bank (0-255)");
                 byte newBIC = Convert.ToByte(Console.ReadLine());
                 if (BankSystem.IsBICTaken(newBIC))
@@ -257,21 +266,21 @@ public static class BankInterface
 }
 public static class BankSystem
 {
-    public static List<Bank> banks = new List<Bank>();
+    public static Dictionary<byte, Bank> banks = new Dictionary<byte, Bank>();
 
-    public static Bank CreateBank(byte bic)
+    public static Bank? CreateBank(byte bic)
     {
-        Bank bank = new Bank(bic);
-        banks.Add(bank);
-        return banks.First(e => e.BIC == bank.BIC);
+        Bank? bank = new Bank(bic);
+        banks.Add(bic, bank);
+        return banks.GetValueOrDefault(bic);
     }
-    public static Bank GetBankByBIC(byte bic)
+    public static Bank? GetBankByBIC(byte bic)
     {
-        return banks.FirstOrDefault(e => e.BIC == bic);
+        return banks.GetValueOrDefault(bic);
     }
     public static bool IsBICTaken(byte BIC)
     {
-        if (banks.FirstOrDefault(e => e.BIC == BIC) != null)
+        if (banks.ContainsKey(BIC))
         {
             return true;
         }
@@ -280,7 +289,7 @@ public static class BankSystem
     public static bool AreBanksAvailable()
     {
         bool isAvailable = false;
-        foreach (Bank bank in banks)
+        foreach (Bank bank in banks.Values)
         {
             if (bank.GetAvailableAccounts() > 0)
             {
@@ -290,22 +299,27 @@ public static class BankSystem
         }
         return isAvailable;
     }
-    public static Account Authenticate(int iban, string password)
+    public static Account? Authenticate(int iban, string? password)
     {
-        Bank bank = GetBankByBIC(BankUtils.GetBICFromIBAN(iban));
+        Bank? bank = GetBankByBIC(BankUtils.GetBICFromIBAN(iban));
         if (bank == null) return null;
 
-        Account account = bank.GetAccountById(BankUtils.GetIDFromIBAN(iban));
-        if (account != null && account.password == password)
+        Account? account = bank.GetAccountById(BankUtils.GetIDFromIBAN(iban));
+        if (password != null && account != null && account.password == password)
         {
             return account;
         }
         return null;
     }
-    public static void PerformTransaction(int senderIBAN, int receiverIBAN, double amount)
+    public static bool PerformTransaction(int senderIBAN, int receiverIBAN, double amount)
     {
-        Bank? senderBank = banks.FirstOrDefault(e => e.BIC == BankUtils.GetBICFromIBAN(senderIBAN));
-        Bank? receiverBank = banks.FirstOrDefault(e => e.BIC == BankUtils.GetBICFromIBAN(receiverIBAN));
+        if (senderIBAN == receiverIBAN)
+        {
+            return false;
+        }
+
+        Bank? senderBank = banks.GetValueOrDefault(BankUtils.GetBICFromIBAN(senderIBAN));
+        Bank? receiverBank = banks.GetValueOrDefault(BankUtils.GetBICFromIBAN(receiverIBAN));
 
         byte senderID = BankUtils.GetIDFromIBAN(senderIBAN);
         byte receiverID = BankUtils.GetIDFromIBAN(receiverIBAN);
@@ -323,7 +337,9 @@ public static class BankSystem
         {
             senderAccount.balance -= amount;
             receiverAccount.balance += amount;
+            return true;
         }
+        return false;
     }
 }
 public static class BankUtils
@@ -353,38 +369,42 @@ public class Bank
     public Bank(byte _BIC)
     {
         BIC = _BIC;
-        accounts = new List<Account>();
+        accounts = new Dictionary<Byte, Account>();
     }
 
     public byte BIC; //Number 0-255
-    List<Account> accounts;
+    Dictionary<Byte, Account> accounts;
 
     public byte GetAvailableAccounts()
     {
         return (byte)(255 - accounts.Count());
     }
-    public Account CreateAccount(string ownerName, string password)
+    public Account? CreateAccount(string? ownerName, string? password)
     {
+        if(ownerName == null || password == null)
+        {
+            return null;
+        }
         Account newAccount = new Account(ownerName, password);
         newAccount.id = (byte)(GetHighestID() + 1);
         int IBAN = BankUtils.CombineToIBAN(BIC, newAccount.id);
         newAccount.IBAN = IBAN;
-        accounts.Add(newAccount);
-        return accounts.First(e => e.id == newAccount.id);
+        accounts.Add(newAccount.id, newAccount);
+        return accounts.GetValueOrDefault(newAccount.id);
     }
-    public Account GetAccountById(byte id)
+    public Account? GetAccountById(byte id)
     {
-        return accounts.FirstOrDefault(e => e.id == id);
+        return accounts.GetValueOrDefault(id);
     }
 
     private byte GetHighestID()
     {
         byte currentID = 0;
-        for (int i = 0; i < accounts.Count; i++)
+        foreach (byte account in accounts.Keys)
         {
-            if (accounts[i].id > currentID)
+            if (account > currentID)
             {
-                currentID = accounts[i].id;
+                currentID = account;
             }
         }
         return currentID;
